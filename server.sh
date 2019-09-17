f51c1cb4533b3726d8fb5a50c58e86307b88d4e7
chenqiliang <chenqiliang@10.55.8.179>
2019-09-17 20:36:25 +0800

    更新脚本

--- server.sh
+++ server.sh
@@ -1,96 +1,191 @@
+#!/bin/sh
+cd "`dirname $0`" || returnerr "$0 is not exsit."
+readonly INSTANCE_NAME=wms-core
+OPER=${1}
+SYS_ID=2107
 
-#!/bin/bash
- 
-cd `dirname $0`
- 
-CUR_SHELL_DIR=`pwd`
-CUR_SHELL_NAME=`basename ${BASH_SOURCE}`
- 
-JAR_NAME="wms-core-0.0.1-SNAPSHOT.jar"
-JAR_PATH=$CUR_SHELL_DIR/$JAR_NAME
- 
-#JAVA_MEM_OPTS=" -server -Xms1024m -Xmx1024m -XX:PermSize=128m"
-JAVA_MEM_OPTS=""
- 
-SPRING_PROFILES_ACTIV="-Dspring.profiles.active="
-#SPRING_PROFILES_ACTIV=""
-LOG_DIR=$CUR_SHELL_DIR/logs
-LOG_PATH=$LOG_DIR/${JAR_NAME%..log
- 
-echo_help()
+JAVA_HOME=/data/jdk1.8.0_152
+DATA_HOME=/data/app
+APP_HOME=$DATA_HOME/$INSTANCE_NAME
+LOG_DIR=$DATA_HOME/logs/$INSTANCE_NAME
+MASTER_OR_SLAVE=master
+JAVA_PID=$LOG_DIR/java.pid
+
+CONF_DIR=$APP_HOME/conf
+LIB_DIR=$APP_HOME/lib
+SYS_DIR=$APP_HOME/apps
+WEB_DIR=$APP_HOME/static
+
+
+
+SYS_HOST=localhost
+JMX_PORT=21071
+#SYS_PORT=10010
+
+SYS_MAIN_CLASS=com.hht.wms.core.WmsCoreApplication
+COMMAND=shutdown
+
+JAVA_OPTS=" -Dfile.encoding=UTF-8  "
+JAVA_OPTS+=" -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+JAVA_OPTS+=" -Xmx1024m -Xms1024m -XX:NewRatio=2 -XX:MaxMetaspaceSize=256m -XX:-UseBiasedLocking -XX:CompileThreshold=20000"
+JAVA_OPTS+=" -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=70 -XX:+UseCMSInitiatingOccupancyOnly"
+JAVA_OPTS+=" -XX:-OmitStackTraceInFastThrow -XX:+ExplicitGCInvokesConcurrent -XX:+ParallelRefProcEnabled"
+JAVA_OPTS+=" -XX:+DisableExplicitGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -Xloggc:${LOG_DIR}/jvm.log"
+JAVA_OPTS+=" -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -XX:ErrorFile=${LOG_DIR}/java_error_%p.log"
+#Custom lib path
+export CLASSPATH=$CONF_DIR:$LIB_DIR/*:$SYS_DIR/*:$WEB_DIR
+# Restrict glibc thread cache count
+export MALLOC_ARENA_MAX=4
+
+#*******fuction define start********
+#error return
+returnerr()
 {
-    echo -e "syntax: sh $CUR_SHELL_NAME start|stop"
-}
- 
-if [ -z $1 ];then
-    echo_help
+    echo $1
     exit 1
-fi
- 
-if [ ! -d "$LOG_DIR" ];then
-    mkdir "$LOG_DIR"
-fi
- 
-if [ ! -f "$LOG_PATH" ];then
-    touch "$LOG_DIR"
-fi
- 
-if [ "$1" == "start" ];then
- 
-    # check server
-    PIDS=`ps --no-heading -C java -f --width 1000 | grep $JAR_NAME | awk '{print $2}'`
-    if [ -n "$PIDS" ]; then
-        echo -e "ERROR: The $JAR_NAME already started and the PID is ${PIDS}."
-        exit 1
+}
+
+# Check start: 0 -> okay, other -> not yet
+checkStart()
+{
+    # ignore stdout/stderr log (e.g. Cannot stat file /proc/xxxx/fd/xxx: No such file or directory)
+#    fuser -s $SYS_PORT/tcp > /dev/null 2>&1
+#    return $?
+	 return 0
+}
+
+# Check stop: 0 -> JVM still running, other -> JVM stopped
+checkExit()
+{
+    fuser -s $JMX_PORT/tcp > /dev/null 2>&1
+    return $?
+}
+
+#help info
+help()
+{
+    echo "Usage: sh ${0} [start|stop|restart|check]"
+    echo "eg: sh ${0} start"
+    exit 0
+}
+
+#start
+start()
+{
+    #if is running,return
+    checkExit
+    if [ $? -eq 0 ]; then
+        echo "${INSTANCE_NAME} is running! Start aborted."
+        return 0
     fi
- 
-    echo "Starting the $JAR_NAME..."
- 
-    # start
-    nohup java $JAVA_MEM_OPTS -jar $SPRING_PROFILES_ACTIV $JAR_PATH >> $LOG_PATH 2>&1 &
- 
-    COUNT=0
-    while [ $COUNT -lt 1 ]; do
-        sleep 1
-        COUNT=`ps  --no-heading -C java -f --width 1000 | grep "$JAR_NAME" | awk '{print $2}' | wc -l`
-        if [ $COUNT -gt 0 ]; then
-            break
+
+    echo "${INSTANCE_NAME} begin to start..."
+    nohup $JAVA_HOME/bin/java -cp $CLASSPATH -Dwebank.sys.id=${SYS_ID} -Dwebank.sys.name=${INSTANCE_NAME} -Dindex.log.home=${LOG_DIR} \
+    ${JAVA_OPTS} ${SYS_MAIN_CLASS} -d ${SYS_HOST} -s ${COMMAND} start >> ${LOG_DIR}/${INSTANCE_NAME}.out 2>&1 &
+	echo  $! > $JAVA_PID
+	
+    echo "${INSTANCE_NAME} pid is: `cat $JAVA_PID`"
+
+    #check
+    loopcount=0
+    echo -n "Binding server listening port"
+    while [ $loopcount -lt 30 ];do
+        checkStart
+        if [ $? -eq 0 ]; then
+            echo " OK!"
+            return 0
+        else
+            echo -n "."
+            sleep 1
+            loopcount=`expr $loopcount + 1`
         fi
     done
-    PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$JAR_NAME" | awk '{print $2}'`
-    echo "${JAR_NAME} Started and the PID is ${PIDS}."
-    echo "You can check the log file in ${LOG_PATH} for details."
- 
-elif [ "$1" == "stop" ];then
- 
-    PIDS=`ps --no-heading -C java -f --width 1000 | grep $JAR_NAME | awk '{print $2}'`
-    if [ -z "$PIDS" ]; then
-        echo "ERROR:The $JAR_NAME does not started!"
-        exit 1
-    fi
- 
-    echo -e "Stopping the $JAR_NAME..."
- 
-    for PID in $PIDS; do
-        kill $PID > /dev/null 2>&1
-    done
- 
-    COUNT=0
-    while [ $COUNT -lt 1 ]; do
-        sleep 1
-        COUNT=1
-        for PID in $PIDS ; do
-            PID_EXIST=`ps --no-heading -p $PID`
-            if [ -n "$PID_EXIST" ]; then
-                COUNT=0
-                break
-            fi
-        done
-    done
- 
-    echo -e "${JAR_NAME} Stopped and the PID is ${PIDS}."
-else
-    echo_help
-    exit 1
-fi
+    echo "timeout(30s)!!! Please check log: ${INSTANCE_NAME}.out!"
+    return 0
+}
 
+#stop
+stop()
+{
+    echo "${INSTANCE_NAME} begin to stop..."
+    checkExit
+    if [ $? -ne 0 ]; then
+        echo "${INSTANCE_NAME} isn't running! Stop aborted."
+        return 0
+    fi
+
+    nohup $JAVA_HOME/bin/java -cp $CLASSPATH -Dwebank.sys.id=${SYS_ID} -Dwebank.sys.name=${INSTANCE_NAME} -Dindex.log.home=${LOG_DIR} \
+        ${SYS_MAIN_CLASS} -d ${SYS_HOST} -p ${SYS_PORT} -s ${COMMAND} stop >> ${LOG_DIR}/${INSTANCE_NAME}.out 2>&1 &
+
+    #check
+    loopcount=0
+    echo -n "Unbinding JMX listening port"
+    while [ $loopcount -lt 30 ];do
+        checkExit
+        if [ $? -ne 0 ]; then
+            echo "OK!"
+			echo "${INSTANCE_NAME} stoped."
+			cat /dev/null>$JAVA_PID
+            return 0
+        else
+            echo -n "."
+            sleep 1
+            loopcount=`expr $loopcount + 1`
+        fi
+    done
+
+    echo "timeout(30s)!!! Please check log: ${INSTANCE_NAME}.out!"
+
+    #force quit
+    fuser -s -k $JMX_PORT/tcp
+    echo "${INSTANCE_NAME} was killed!"
+	kill -9 `cat $JAVA_PID`
+	echo  "${INSTANCE_NAME} killed success!"
+	cat /dev/null>$JAVA_PID
+    return 0
+}
+
+restart()
+{
+    stop
+    start
+    exit 0
+}
+
+#check
+#return valus: 0-running, 1-not running
+check()
+{
+    checkExit
+    if [ $? -eq 0 ]; then
+        echo "${INSTANCE_NAME} is running."
+        return 1
+    else
+        echo "${INSTANCE_NAME} isn't running"
+        return 0
+    fi
+}
+#*******fuction define end********
+
+#check dir & file
+[ -d $LOG_DIR ] || mkdir -p $LOG_DIR || returnerr '$OPER fail: make log dir($LOG_DIR) fail!'
+[ -d $DATA_HOME/files/$INSTANCE_NAME ] || mkdir -p $DATA_HOME/files/$INSTANCE_NAME || returnerr '$OPER fail: make files dir($LOG_DIR) fail!'
+
+#do operation
+case "${1}" in
+start)
+    start
+    ;;
+stop)
+    stop
+    ;;
+restart)
+    restart
+    ;;
+check)
+    check
+    ;;
+*)
+    help
+    ;;
+esac
