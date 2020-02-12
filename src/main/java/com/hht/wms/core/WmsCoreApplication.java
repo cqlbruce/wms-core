8db2044a25dbbef2e3fc60495b3e8016ec4a0041
java07070620 <cqlbruce@gmail.com>
2020-02-12 22:09:52 +0800

    更新

--- src/main/java/com/hht/wms/core/WmsCoreApplication.java
+++ src/main/java/com/hht/wms/core/WmsCoreApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WmsCoreApplication {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WmsCoreApplication.class, args);
 	}
